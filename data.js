daf0df9c62febceb0f07762b711037e65fae2e2e
lyumn <meining@bookacoach.com>
2015-10-14 21:24:29 -0400

    总之先把藏干给放好位置

---
+++ data.js
@@ -0,0 +1,17 @@
+//八字
+//var nian_gan;
+//var nian_zhi;
+//var yue_gan;
+//var yue_zhi;
+//var ri_zhu;
+//var ri_zhi;
+//var shi_gan;
+//var shi_zhi;
+var 年干;
+var 年支;
+var 月干;
+var 月支;
+var 日主;
+var 日支;
+var 时干;
+var 时支;
